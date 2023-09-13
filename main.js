b49a042741ccf334c88949a6a3ded5d801a69328
Marcos <marcos.sanchez@saei.mx>
2023-09-12 20:18:56 -0500

    "control OSC por webcam"

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as handTrack from "handtrackjs";
 const video = document.getElementById("webcam");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+const volumeSlider = document.getElementById("vol");
 
 const modelParams = {
   flipHorizontal: true,
@@ -32,14 +33,27 @@ function loadModelAndRunDetection() {
   });
 }
 
+let previousX = null; // Variable para almacenar la posición anterior en X de la mano
+
 function runDetection(model) {
   model.detect(video).then((predictions) => {
-    if (predictions.length && predictions[0].label === "closed") {
-      sendOscMessage();
+    if (predictions.length) {
+      const hand = predictions[0];
+
+      if (hand.label === "open") {
+        const handPositionX = hand.bbox[0] + hand.bbox[2] / 2;
+        const relativePosition = handPositionX / video.width;
+        volumeSlider.value = Math.min(
+          1,
+          Math.max(0, relativePosition.toFixed(3))
+        );
+
+        sendOscWithValue(volumeSlider.value); // Llamada a la función para enviar el mensaje OSC
+      }
     }
 
     model.renderPredictions(predictions, canvas, context, video);
-    requestAnimationFrame(() => runDetection(model)); // Pasamos el modelo como argumento
+    requestAnimationFrame(() => runDetection(model));
   });
 }
 
