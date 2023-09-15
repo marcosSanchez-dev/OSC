ba8edb36546c22bec478afeb97f444a7a7b02256
Marcos <marcos.sanchez@saei.mx>
2023-09-15 13:26:16 -0500

    "manejo de colores y  linewidht por dos puertos"

--- server.js
+++ server.js
@@ -10,14 +10,23 @@ app.use(express.static("public"));
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const oscClient = new osc.Client("127.0.0.1", 8010);
+const oscClient8010 = new osc.Client("127.0.0.1", 8010);
+const oscClient7099 = new osc.Client("127.0.0.1", 7099);
+
 const oscServer = new osc.Server(7099, "0.0.0.0");
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     try {
       const oscMessage = JSON.parse(message);
-      oscClient.send(oscMessage.address, ...oscMessage.args);
+
+      if (oscMessage.address.startsWith("/fixtures/Group-1/color")) {
+        // Enviar mensajes de colores al puerto 8010
+        oscClient8010.send(oscMessage.address, ...oscMessage.args);
+      } else {
+        // Otros mensajes se envían al puerto 7099
+        oscClient7099.send(oscMessage.address, ...oscMessage.args);
+      }
     } catch (error) {
       console.error("Error al procesar el mensaje:", error);
     }
@@ -28,10 +37,10 @@ server.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
 });
 
-oscServer.on("message", function (msg, rinfo) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ address: msg[0], value: msg[1] }));
-    }
-  });
-});
+// oscServer.on("message", function (msg, rinfo) {
+//   wss.clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(JSON.stringify({ address: msg[0], value: msg[1] }));
+//     }
+//   });
+// });
